4caf7972b77ec563ed606ea8aa0d24c163c72189
saitanya <ksaitanya@gmail.com>
2019-02-21 00:21:26 -0600

    Updated with project_6

--- README.md
+++
@@ -1,2 +0,0 @@
-# project
-Human Computer Interaction Project
--- project.js
+++ project.js
@@ -1,1 +1,20 @@
-var tileRef=document.getElementsByClassName("tile");
+// Questionaire
+
+var radiosRef = document.getElementById('answers');
+var checked;
+function nextPage(myurl){
+    var radios = document.getElementsByName('test');
+    checked = false;
+    for (var i = 0; i < radios.length; i++) {
+        if (radios[i].type === 'radio' && radios[i].checked) {
+            checked = true;
+            break;
+        }
+    }
+    if(!checked){
+        alert('Please select an option');
+    }
+    else{
+        window.location = myurl;
+    }
+}
